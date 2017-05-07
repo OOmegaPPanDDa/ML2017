b6350391bc1171b445d8b4b58703a5c69c5bf58e
OOmegaPPanDDa <b02705027@ntu.edu.tw>
2017-05-08 01:33:54 +0800

    wget dropbox dl=1

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://www.dropbox.com/s/0pi1opu7jrq71hr/model.h5?dl=0# -O model.h5
+wget https://www.dropbox.com/s/0pi1opu7jrq71hr/model.h5?dl=1# -O model.h5
 python3 hw3_test.py $1 $2
