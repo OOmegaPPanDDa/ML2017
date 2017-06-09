c767add92edfbf431cfcb98fc4345d5d5f043c5f
OOmegaPPanDDa <b02705027@ntu.edu.tw>
2017-06-09 19:34:14 +0800

    bash python3.5

--- hw6/hw6.sh
+++ hw6/hw6.sh
@@ -4,4 +4,4 @@ user_path="$1/users.csv"
 movie_path="$1/movies.csv"
 test_path="$1/test.csv"
 
-python3 hw6_test.py $test_path $2
+python3.5 hw6_test.py $test_path $2
--- hw6/hw6_best.sh
+++ hw6/hw6_best.sh
@@ -4,4 +4,4 @@ user_path="$1/users.csv"
 movie_path="$1/movies.csv"
 test_path="$1/test.csv"
 
-python3 hw6_best_test.py $user_path $movie_path $test_path $2
+python3.5 hw6_best_test.py $user_path $movie_path $test_path $2
