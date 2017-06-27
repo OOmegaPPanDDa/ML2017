4c1a0ad559d4ed55ae910eee78e2aa7077f62439
OOmegaPPanDDa <b02705027@ntu.edu.tw>
2017-06-28 05:40:16 +0800

    final model1

--- final/src/howard_code/lon_lat_pred.py
+++ final/src/howard_code/lon_lat_pred.py
@@ -5,9 +5,6 @@ Created on Sat Jun 24 20:14:03 2017
 @author: HSIN
 """
 
-import pandas as pd
-import time
-import xgboost as xgb
 from keras.engine import Layer
 from keras.layers import Input
 from keras.layers.advanced_activations import LeakyReLU
@@ -17,11 +14,12 @@ from keras.layers.embeddings import Embedding
 from keras.optimizers import Adam,SGD
 from keras.models import Model
 from keras.callbacks import EarlyStopping, ModelCheckpoint
+import pandas as pd
+import time
+import xgboost as xgb
+import keras
 
 stime = time.time()
-
-
-# XGB
 
 train = pd.read_csv('./train_lon_lat_missing.csv', index_col=0)
 test = pd.read_csv('./test_lon_lat_missing.csv', index_col=0)
@@ -37,6 +35,21 @@ lat_test = pd.read_csv('./lat_test.csv', index_col=0)
 lat_target = pd.read_csv('./lat_target.csv', index_col=0, header=None)
 
 
+def get_nn_model():
+    user_input = Input(shape=[6])
+    hidden = Dense(12,activation='relu')(user_input)
+    hidden = Dense(36,activation='relu')(hidden)
+    hidden = Dense(36,activation='relu')(hidden)
+    hidden = Dense(18,activation='relu')(hidden)
+    output = Dense(12,activation='relu')(hidden)
+    output = Dense(4,activation='relu')(hidden)
+    output = Dense(1)(hidden)
+    model = Model(user_input,output)
+    adam = Adam(lr=0.06, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+    model.compile(loss='mse', optimizer=adam)
+    model.summary()
+    return model
+'''
 # lon xgb
 lon_est = xgb.XGBRegressor(
                             max_depth = 15,
@@ -67,72 +80,11 @@ lat_est = xgb.XGBRegressor(
                             
 lat_est.fit(lat_train, lat_target)
 lat_pred = lat_est.predict(lat_test) 
-
-
-# get train test index
-train_ind = lon_test.index[lon_test.index < train.shape[0]]
-test_ind = lon_test.index[lon_test.index >= train.shape[0]]
-
-
-# replace train lon lat by predicted value
-train.ix[train_ind,'longitude'] = lon_pred[0:len(train_ind)] 
-train.ix[train_ind,'latitude' ]= lat_pred[0:len(train_ind)]
-
-# replace test lon lat by predicted value
-test.ix[test_ind,'longitude'] = lon_pred[len(train_ind):lon_pred.shape[0]] 
-test.ix[test_ind,'latitude'] = lat_pred[len(train_ind):lat_pred.shape[0]]
-
-      
-
-train.to_csv('train_lon_lat_xgb_predicted.csv')
-test.to_csv('test_lon_lat_xgb_predicted.csv')
-
-
-
-
-
-
-
-
-
-# DNN
-
-
-
-train = pd.read_csv('./train_lon_lat_missing.csv', index_col=0)
-test = pd.read_csv('./test_lon_lat_missing.csv', index_col=0)
-
-# to predict lon
-lon_train = pd.read_csv('./lon_train.csv',index_col=0)
-lon_test = pd.read_csv('./lon_test.csv',index_col=0)
-lon_target = pd.read_csv('./lon_target.csv',index_col=0, header=None)
-
-# to predict lat
-lat_train = pd.read_csv('./lat_train.csv', index_col=0)
-lat_test = pd.read_csv('./lat_test.csv', index_col=0)
-lat_target = pd.read_csv('./lat_target.csv', index_col=0, header=None)
-
-
-def get_nn_model():
-    user_input = Input(shape=[6])
-    hidden = Dense(12,activation='relu')(user_input)
-    hidden = Dense(36,activation='relu')(hidden)
-    hidden = Dense(36,activation='relu')(hidden)
-    hidden = Dense(18,activation='relu')(hidden)
-    output = Dense(12,activation='relu')(hidden)
-    output = Dense(4,activation='relu')(hidden)
-    output = Dense(1)(hidden)
-    model = Model(user_input,output)
-    adam = Adam(lr=0.06, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-    model.compile(loss='mse', optimizer=adam)
-    model.summary()
-    return model
-    
-    
+'''
 epoch_nb = 10000
 
 earlystopping = EarlyStopping(monitor='loss', patience = 100., verbose=1, mode='min')
-checkpoint = ModelCheckpoint(filepath='lon_tmp_best.hdf5',
+checkpoint = ModelCheckpoint(filepath='tmp.hdf5',
                              verbose=1,
                              save_best_only=True,
                              save_weights_only=True,
@@ -142,11 +94,11 @@ checkpoint = ModelCheckpoint(filepath='lon_tmp_best.hdf5',
 lon_model = get_nn_model()
 lon_model.fit(lon_train.as_matrix(), lon_target.as_matrix(),batch_size=5000,
               epochs=epoch_nb,callbacks=[earlystopping,checkpoint])
-lon_model.load_weights('lon_tmp_best.hdf5')
+lon_model.load_weights('tmp.hdf5')
 lon_pred = lon_model.predict(lon_test.as_matrix()) 
 
 earlystopping = EarlyStopping(monitor='loss', patience = 100., verbose=1, mode='min')
-checkpoint = ModelCheckpoint(filepath='lat_tmp_best.hdf5',
+checkpoint = ModelCheckpoint(filepath='tmp.hdf5',
                              verbose=1,
                              save_best_only=True,
                              save_weights_only=True,
@@ -156,7 +108,7 @@ checkpoint = ModelCheckpoint(filepath='lat_tmp_best.hdf5',
 lat_model = get_nn_model()
 lat_model.fit(lat_train.as_matrix(), lat_target.as_matrix(),batch_size=5000,
               epochs=epoch_nb,callbacks=[earlystopping,checkpoint])
-lat_model.load_weights('lat_tmp_best.hdf5')
+lat_model.load_weights('tmp.hdf5')
 lat_pred = lat_model.predict(lat_test.as_matrix()) 
 
 lon_model.save('lon_nn_model.h5')
